bf119e2535bd7174a496db949e5976e3f8079f9e
ArCan314 <39261650+ArCan314@users.noreply.github.com>
2020-06-06 22:44:34 +0800

    添加房间号至IP地址和端口的接口

--- CommonLib/common.h
+++ CommonLib/common.h
@@ -22,23 +22,53 @@ enum class SpeedLevel
 
 int getRandomInt(int low_bound, int up_bound);
 
-class Config
-{
-public:
+namespace Config {
+
+    // 标记主机的地址和端口，用于从控机发送请求给中央空调
     static constexpr const char *kMasterHostAddr = "localhost";
     static constexpr quint16 kMasterListenPort = 12345;
+
+    // 设定传输失败时重传次数
     static constexpr int kRetryAttempt = 3;
 
+    /**
+     * @brief 获取 Listener 监听的端口
+     * 
+     * @return quint16 监听的端口值
+     */
     static quint16 getSlaveListenerPortValue();
+
+    /**
+     * @brief 设定 Listener 监听的端口
+     * 
+     * @param port 监听的端口
+     */
     static void setSlaveListenerPort(const quint16 port);
+
+    /**
+     * @brief 判断是否设定了 Listener 的端口
+     * 
+     * @return true 已设定
+     * @return false 未设定，需要至少调用一次 setSlaveListenerPort(port)
+     */
     static bool hasSlaveListenerPort();
 
-private:
-    static std::optional<quint16> slave_listener_port;
+    /**
+     * @brief 将从控机与其地址和端口进行对应
+     * 
+     * @param room_id 从控机所在房间号
+     * @param host_addr 该从控机的地址
+     * @param port 该从控机监听的端口
+     */
+    static void addAddress(const QString &room_id, const QString &host_addr, const quint16 port);
 
+    /**
+     * @brief 返回给定房间号的地址和监听端口
+     * 
+     * @param room_id 从控机所在房间号
+     * @return std::pair<bool, std::pair<QString, quint16>> 若存在该房间号对应的记录，则返回 {true, {room_id, port}}, 
+     * 否则返回 {false, {}}
+     */
+    static std::pair<bool, std::pair<QString, quint16>> getAddress(const QString &room_id);
 };
-
-
-
-
 #endif // COMMON_H
--- CommonLib/config.cpp
+++ CommonLib/config.cpp
@@ -5,29 +5,46 @@
 #include <shared_mutex>
 #include <mutex>
 
+#include <QString>
 
-std::optional<quint16> Config::slave_listener_port = std::nullopt;
-static std::shared_mutex rw_mutex;
-
-quint16 Config::getSlaveListenerPortValue()
+namespace Config
 {
-    std::shared_lock lock(rw_mutex);
-    assert(slave_listener_port.has_value());
-    return slave_listener_port.value();
-}
+    static std::optional<quint16> slave_listener_port = std::nullopt;
+    static std::map<QString, std::pair<QString, quint16>> roomid_to_address{};
 
-void Config::setSlaveListenerPort(const quint16 port)
-{
-    std::unique_lock lock(rw_mutex);
-    slave_listener_port = port;
-}
+    static std::shared_mutex slave_port_rw_mutex;
+    static std::shared_mutex roomid_to_addr_rw_mutex;
 
-bool Config::hasSlaveListenerPort()
-{
-    std::shared_lock lock(rw_mutex);
-    return slave_listener_port.has_value();
-}
+    quint16 getSlaveListenerPortValue()
+    {
+        std::shared_lock lock(slave_port_rw_mutex);
+        assert(slave_listener_port.has_value());
+        return slave_listener_port.value();
+    }
 
+    void setSlaveListenerPort(const quint16 port)
+    {
+        std::unique_lock lock(slave_port_rw_mutex);
+        slave_listener_port = port;
+    }
 
+    bool hasSlaveListenerPort()
+    {
+        std::shared_lock lock(slave_port_rw_mutex);
+        return slave_listener_port.has_value();
+    }
 
+    void addAddress(const QString &room_id, const QString &host_addr, const quint16 port)
+    {
+        std::unique_lock lock(roomid_to_addr_rw_mutex);
+        roomid_to_address.at(room_id) = {host_addr, port};
+    }
 
+    std::pair<bool, std::pair<QString, quint16>> getAddress(const QString &room_id)
+    {
+        std::shared_lock lock(roomid_to_addr_rw_mutex);
+        if (auto iter = roomid_to_address.find(room_id); iter != roomid_to_address.end())
+            return {true, (*iter).second};
+        return {false, {}};
+    }
+} // namespace Config
