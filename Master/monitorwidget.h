b5fb155923d07fae559c69493741eed328bde419
giriawsh <giriawsh@outlook.com>
2020-06-13 16:51:06 +0800

    new version update

---
+++ Master/monitorwidget.h
@@ -0,0 +1,38 @@
+﻿#ifndef MONITORWIDGET_H
+#define MONITORWIDGET_H
+
+
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QStandardItem>
+#include <QStandardItemModel>
+#include <QTreeView>
+#include <QTableView>
+#include <QHeaderView>
+
+namespace Ui {
+class MonitorWidget;
+}
+
+class MonitorWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit MonitorWidget(QWidget *parent = nullptr);
+//    ~MonitorWidget();
+    void createView();
+
+private:
+    Ui::MonitorWidget *ui;
+
+    QPushButton *testBtn;
+    QPushButton *m_PushButton;
+    QVBoxLayout *mainLayout;
+//    QTreeView *treeView;
+    QStandardItemModel *standItemModel;
+    QTableView *tableView;
+};
+
+#endif // MONITORWIDGET_H
---
+++ Master/parameterwidget.cpp
@@ -0,0 +1,54 @@
+﻿#include "parameterwidget.h"
+#include "ui_parameterwidget.h"
+
+ParameterWidget::ParameterWidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::ParameterWidget)
+{
+    ui->setupUi(this);
+    setAttribute(Qt::WA_DeleteOnClose);
+    this->setWindowTitle("参数设置");
+    this->setFixedSize(this->width(),this->height());
+    connect(ui->pushButton_cancel,&QPushButton::clicked,this,&ParameterWidget::cancel);
+    connect(ui->pushButton_confirm,&QPushButton::clicked,this,&ParameterWidget::confirm);
+}
+
+ParameterWidget::~ParameterWidget()
+{
+    delete ui;
+}
+
+void ParameterWidget::cancel()
+{
+    emit cancel_signal();
+    this->hide();
+}
+
+void ParameterWidget::confirm()
+{
+    this->temperature = ui->lineEdit_temperature->text().toInt();
+    if(ui->radioButton_hot->isChecked())
+    {
+        this->mode = "hot";
+        ui->lineEdit_temperature->setText("28");
+    }else{
+        this->mode = "cold";
+        ui->lineEdit_temperature->setText("22");
+    }
+    //TODO 控制从控机mode and temperature
+    emit confirm_signal();
+    this->hide();
+}
+
+int ParameterWidget::getTemperature()
+{
+    return temperature;
+}
+
+QString ParameterWidget::getMode()
+{
+//     qDebug() << "Mode:" << mode;
+    return mode;
+}
+
+
