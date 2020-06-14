23b8cd6d9742548d8770cb13a2d980ab33adbcc8
994503782 <38678289+994503782@users.noreply.github.com>
2020-06-14 14:06:15 +0800

    简化结构，完成report调用接口

--- Master/report.cpp
+++ Master/report.cpp
@@ -5,22 +5,47 @@ Report::Report()
 
 }
 
-std::pair<bool,std::vector<Detail>> Report::getDetails(const QDateTime &begin,const QDateTime &end,const QString &roomID)
+std::pair<bool,std::vector<StatPayload>> Report::getDetails(const QDateTime &begin,const QDateTime &end,const QString &roomID)
 {
-
+    std::pair<bool, std::vector<StatPayload>> response = db.getRoomRequestStats(begin,end);//获取当日报表
+    if(response.first == false)return response;
+    for(auto it = response.second.begin(); it != response.second.end();)
+    {
+        if(it->room_id != roomID)//去除不需要的详单
+        {
+            response.second.erase(it);
+        }else
+        {
+            it++;
+        }
+    }
+    return response;
 }
 
 bool Report::getTodayReport()
 {
+    QDateTime begin, end;
+    begin.setDate(QDate::currentDate());
+    end.setDate(QDate::currentDate().addDays(1));
+    std::pair<bool, std::vector<StatPayload>> response = db.getRoomRequestStats(begin,end);//获取当日报表
+    if(response.first == false)return false;
+    _details.clear();
+    for(auto it = response.second.begin(); it != response.second.end(); it++)//将报表按照房间号拆分
+    {
+        _details.insert(pair<QString,std::vector<StatPayload>>(it->room_id, *it));
+    }
 
 }
 
-void Report::writeDetail(const Detail &detail)
+bool Report::writeDetail(const StatPayload &detail)
 {
-
+    return db.addRoomRequestStat(detail);
 }
 
-std::vector<Detail> getRoomReport(const QString &roomID)
+std::vector<StatPayload> Report::getRoomReport(const QString &roomID)
 {
-
+    auto it = _details.find(roomID);
+    if(it != _details.end())return it->secend;
+    std::vector<StatPayload> result;
+    return result;
 }
